376c4cb9ab4189621532f7f5a4ded5ef48ff0996
RetroCoder13 <archie.anthony@blueyonder.co.uk>
2023-09-23 13:54:47 +0100

    Added brushWidth and some other functions. One gets the state of the newest data when you select "Stream", this doesn't load the whole board though!

--- main.js
+++ main.js
@@ -5,23 +5,39 @@ var mouseDown = false
 var updatedPositions = []
 var enableDrawing = true
 
-window.onload = async function(){
+async function getScreen(){
     var { data, error } = await supabaseClient
         .from('boardData')
         .select()
     
-    console.log(data)
+    console.log(data[0].boardData)
+    
+    if(data[0].boardData == "clear"){
+        ctx.clearRect(0,0,canvas.width,canvas.height)
+    } else {
+        data = JSON.parse(data[0].boardData)
+        brushWidth = data[1]
+        ctx.fillStyle = data[0]
+        for(let i=2;i<data.length;i++){
+            ctx.fillRect(data[i][0]-(brushWidth/2),data[i][1]-(brushWidth/2),brushWidth,brushWidth)
+        }
+    }
 }
 
 function changeMode(){
     if(document.getElementById('mode').value == "send"){
         document.getElementById('color').hidden = false
+        document.getElementById('clearScreen').hidden = false
+        document.getElementById('brushWidth').hidden = false
         enableDrawing = true
     }
 
     else{
         document.getElementById('color').hidden = true
+        document.getElementById('clearScreen').hidden = true
+        document.getElementById('brushWidth').hidden = true
         enableDrawing = false
+        getScreen()
         supabaseClient
             .channel('any')
             .on('postgres_changes', { event: 'UPDATE', schema: 'public', table: 'boardData', filter: 'user_id=eq.1' }, payload => {
@@ -29,9 +45,10 @@ function changeMode(){
                     ctx.clearRect(0,0,canvas.width,canvas.height)
                 } else {
                     payload = JSON.parse(payload.new.boardData)
+                    brushWidth = payload[1]
                     ctx.fillStyle = payload[0]
-                    for(let i=1;i<payload.length;i++){
-                        ctx.fillRect(payload[i][0],payload[i][1],10,10)
+                    for(let i=2;i<payload.length;i++){
+                        ctx.fillRect(payload[i][0]-(brushWidth/2),payload[i][1]-(brushWidth/2),brushWidth,brushWidth)
                     }
                 }
             })
@@ -42,17 +59,18 @@ function changeMode(){
 canvas.addEventListener('mousedown',function mouseDownEvent(e){
     if(enableDrawing){
         ctx.fillStyle = document.getElementById('color').value
+        brushWidth = document.getElementById('brushWidth').value
         mouseDown = true
-        updatedPositions = [ctx.fillStyle,[e.offsetX,e.offsetY]]
-        ctx.fillRect(e.offsetX,e.offsetY,10,10)
+        updatedPositions = [ctx.fillStyle,brushWidth,[e.offsetX-(brushWidth/2),e.offsetY-(brushWidth/2)]]
+        ctx.fillRect(e.offsetX-(brushWidth/2),e.offsetY-(brushWidth/2),brushWidth,brushWidth)
     }
 })
 
 canvas.addEventListener('mousemove',function mouseMoveEvent(e){
     if(enableDrawing){
-        updatedPositions.push([e.offsetX,e.offsetY])
         if(mouseDown){
-            ctx.fillRect(e.offsetX,e.offsetY,10,10)
+            updatedPositions.push([e.offsetX-(brushWidth/2),e.offsetY-(brushWidth/2)])
+            ctx.fillRect(e.offsetX-(brushWidth/2),e.offsetY-(brushWidth/2),brushWidth,brushWidth)
         }
     }
 })
