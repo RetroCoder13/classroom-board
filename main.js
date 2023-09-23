6a0c67c0678c66b7233907abdb43481a3389ad25
RetroCoder13 <archie.anthony@blueyonder.co.uk>
2023-09-23 13:26:25 +0100

    Working board. Need to add clear function

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const canvas = document.getElementById('board')
 const ctx = canvas.getContext('2d')
 
 var mouseDown = false
+var updatedPositions = []
+var enableDrawing = true
 
 window.onload = async function(){
     var { data, error } = await supabaseClient
@@ -13,49 +15,56 @@ window.onload = async function(){
 
 function changeMode(){
     if(document.getElementById('mode').value == "send"){
-        document.addEventListener('mousedown',function(e){
-            mouseDown = true
-            ctx.fillRect(e.offsetX,e.offsetY,10,10)
-        })
-
-        document.addEventListener('mousemove',function(e){
-            if(mouseDown){
-                ctx.fillRect(e.offsetX,e.offsetY,10,10)
-            }
-        })
-
-        document.addEventListener('mouseup',async function(e){
-            mouseDown = false
-            imageData = ctx.getImageData(0,0,canvas.width,canvas.height).data
-            finalImageData = {}
-            for(let i=0;i<imageData.length/4;i++){
-                if(imageData[4*i] != 0 || imageData[4*i+1] != 0 || imageData[4*i+2] != 0 || imageData[4*i+3] != 0){
-                    finalImageData[i] = [imageData[4*i],imageData[4*i+1],imageData[4*i+2],imageData[4*i+3]]
-                }
-            }
-            var {error} = await supabaseClient
-                .from('boardData')
-                .update({boardData: JSON.stringify(finalImageData)})
-                .eq('user_id', 1)
-        })
+        document.getElementById('color').hidden = false
+        enableDrawing = true
     }
 
     else{
+        document.getElementById('color').hidden = true
+        enableDrawing = false
         supabaseClient
             .channel('any')
             .on('postgres_changes', { event: 'UPDATE', schema: 'public', table: 'boardData', filter: 'user_id=eq.1' }, payload => {
                 payload = JSON.parse(payload.new.boardData)
                 console.log(payload)
-                imageData = ctx.createImageData(canvas.width,canvas.height)
-                console.log(parseInt(Object.keys(payload)[0]), payload[parseInt(Object.keys(payload)[0])])
-                for(let i=0;i<Object.keys(payload).length;i++){
-                    imageData[parseInt(Object.keys(payload)[i])] = payload[parseInt(Object.keys(payload)[i])]
+                ctx.fillStyle = payload[0]
+                for(let i=1;i<payload.length;i++){
+                    ctx.fillRect(payload[i][0],payload[i][1],10,10)
                 }
-                console.log(imageData)
-                ctx.putImageData(imageData,0,0)
             })
             .subscribe()
     }
 }
+
+canvas.addEventListener('mousedown',function mouseDownEvent(e){
+    if(enableDrawing){
+        ctx.fillStyle = document.getElementById('color').value
+        mouseDown = true
+        updatedPositions = [ctx.fillStyle,[e.offsetX,e.offsetY]]
+        ctx.fillRect(e.offsetX,e.offsetY,10,10)
+    }
+})
+
+canvas.addEventListener('mousemove',function mouseMoveEvent(e){
+    if(enableDrawing){
+        updatedPositions.push([e.offsetX,e.offsetY])
+        if(mouseDown){
+            ctx.fillRect(e.offsetX,e.offsetY,10,10)
+        }
+    }
+})
+
+canvas.addEventListener('mouseup',async function mouseUpEvent(e){
+    if(enableDrawing){
+        mouseDown = false
+        updatedPositions = Array.from(new Set(updatedPositions.map(JSON.stringify)), JSON.parse)
+        console.log(updatedPositions)
+        var {error} = await supabaseClient
+            .from('boardData')
+            .update({boardData: updatedPositions})
+            .eq('user_id', 1)
+        console.log(error)
+    }
+})
 
 changeMode()
